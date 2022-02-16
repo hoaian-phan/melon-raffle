5ebc90f33912fef12fc147b401f32b4b73b02ec2
Hoai An Phan <pnhoaian2904@gmail.com>
2022-02-16 07:04:08 +0000

    Completed version 1

--- raffle.py
+++ raffle.py
@@ -42,7 +42,7 @@ def get_customers_from_file(customer_file_path):
 def pick_winner(customers):
     """Choose a random winner from list of customers."""
 
-    chosen_customer = random.choice(customers)
+    chosen_customer = choice(customers)
 
     name = chosen_customer.name
     email = chosen_customer.email
@@ -55,3 +55,5 @@ def run_raffle():
 
     customers = get_customers_from_file("customers.txt")
     pick_winner(customers)
+
+run_raffle()
